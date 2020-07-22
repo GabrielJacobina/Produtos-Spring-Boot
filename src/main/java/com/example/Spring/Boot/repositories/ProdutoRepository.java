7f64de8bd1d4a0a2d150ee20004113ca776b6096
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2020-07-22 11:04:58 -0300

    firt commit

---
+++ src/main/java/com/example/Spring/Boot/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.example.Spring.Boot.repositories;
+
+import com.example.Spring.Boot.models.ProdutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+
+}
